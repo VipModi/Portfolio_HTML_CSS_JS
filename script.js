c48ae16c9200aa7a92b421f69fb90308d68000b5
Vipul Modi <vmodi00@gmail.com>
2024-09-03 02:10:38 +0530

    Day 4
    updated profile section and following exact tutorial to avoid mistakes

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-/**
- * Toggles the mobile menu on and off
- * @param {Event} event The event that triggered the function
- */
 function toggleMenu() {
-    // Get the menu and the hamburger icon
-    const menu = document.querySelector(".menu_links");
-    const icon = document.querySelector(".hamburger_icon");
-
-    // Toggle the "open" class on both elements
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
-}
+  }
